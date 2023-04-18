d6335f6e198d880ed7bdc86b33a09f791bbd8f80
swapnilChourasiya <swapnilchofficial@gmail.com>
2023-04-19 01:25:14 +0530

    contributors css and graphs have been added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
 import ParseExcel from "./Components/parseExcel.js";
 import Navbar from "./Components/navbar.js";
-import Theory from "./Components/theory.js";
+import Contributors from "./Components/contributors.js";
+// import Theory from "./Components/theory.js";
+
 
 import './App.css' ;
 import { Route, Routes } from "react-router-dom";
@@ -15,7 +17,8 @@ function App() {
       <Navbar/>
     <Routes>
       <Route exact path="/" element={<ParseExcel/>} />
-      <Route exact path="/theory" element={<Theory/>} />
+      {/* <Route exact path="/theory" element={<Theory/>} /> */} 
+      <Route exact path="/contributors" element={<Contributors/>} />
     </Routes> 
     </div>
     </>
--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -7,10 +7,13 @@ import {Link} from 'react-router-dom'
 const Navbar = () => {
 
   return (
+    <div className="dummy">
     <div className = 'Navbar-wrapper'>
 	   	<div className = 'group'>Group 20</div>
 	   	<Link className="labelname"  to="/">HOME</Link>
-	   	<Link className="labelname"  to="/theory">THEORY</Link>
+	   	{/* <Link className="labelname"  to="/theory">THEORY</Link> */}
+	   	<Link className="labelname"  to="/contributors">CONTRIBUTORS</Link>
+    </div>
     </div>
   );
 }
